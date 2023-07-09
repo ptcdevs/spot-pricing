6eafa4ad8d284c291394015a1258f4ea7e03fbf5
David Gallmeier <dg@xounges.net>
2023-07-08 23:05:43 -0400

    Add .net console project for aws ec2

---
+++ csharp/aws-console/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Amazon;
+using Amazon.EC2;
+using Amazon.Runtime;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("start");
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddEnvironmentVariables()
+    .Build();
+
+using var awsClient = new AmazonEC2Client(
+    new BasicAWSCredentials("AKIAXULOAMFSPPU3LNWM", "RGnVXtRv+U0essdPi1quU0HRvoCkgxE2t76iFU/V"),
+    RegionEndpoint.USEast1);
+var spotPriceHistoryAsync = await awsClient.DescribeSpotPriceHistoryAsync();
+
+Console.WriteLine("fin");
