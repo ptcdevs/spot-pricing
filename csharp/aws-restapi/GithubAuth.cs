6f245eece94bc35c77400cd959decd135f60c42c
David Gallmeier <dg@xounges.net>
2023-07-15 00:14:33 -0400

    feat: new restapi (#1)
    * Add new asp.net project
    
    * Add github oauth
    
    * Refactor github auth
    
    * Delete old code
    
    * Update github auth config
    
    start adding to swaggergen and ui
    
    * Swagger oauth2 doc good enough
    
    * Clean up warnings
    
    * Add comments
    
    * Add releaser action
    
    * Update releaser
    
    * Update releaser
    
    * Update releaser
    
    * Update releaser
    
    * Update releaser
    
    * Update releaser
    
    * Update releaser
    
    * Update releaser
    
    * Enable release please
    
    * Cleanup release action

---
+++ csharp/aws-restapi/GithubAuth.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace aws_restapi;
+
+public abstract class GithubAuth
+{
+    /// <summary>
+    /// this is a custom github policy, whitelisting specific users; moved here to declutter Program.cs
+    /// </summary>
+    /// <returns></returns>
+    public static Action<AuthorizationOptions> CustomPolicy() =>
+        options =>
+        {
+            options.AddPolicy("ValidGithubUser", new AuthorizationPolicyBuilder()
+                .RequireAssertion(context =>
+                {
+                    var authorizedGithubUsers = new[]
+                    {
+                        "vector623",
+                    };
+
+                    var githubUser = context.User.Claims
+                        .Where(claim => claim.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"))
+                        .Select(claim => claim.Value)
+                        .SingleOrDefault() ?? "";
+                    Console.WriteLine($"github user: {githubUser}");
+
+                    return authorizedGithubUsers.Contains(githubUser);
+                }).Build());
+        };
+
+    /// <summary>
+    /// this class decorates the swagger doc (and ui) with oauth labels for methods requiring authorization
+    /// </summary>
+    public class SecurityFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var validGithubUserAuth = context
+                .ApiDescription
+                .ActionDescriptor
+                .EndpointMetadata
+                .OfType<AuthorizeAttribute>()
+                .Any(authAttr => authAttr.Policy is "ValidGithubUser");
+
+            var githubCustomRequirement = new List<OpenApiSecurityRequirement>
+            {
+                new()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "GithubOAuth"
+                            }
+                        },
+                        new List<string>()
+                    }
+                },
+            };
+            var noAuth = new List<OpenApiSecurityRequirement>();
+            operation.Security = validGithubUserAuth
+                ? githubCustomRequirement
+                : noAuth;
+        }
+    }
+}
---
+++ csharp/aws-restapi/Program.cs
@@ -0,0 +1,79 @@
+using aws_restapi;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Events;
+
+#region config
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(builder.Environment.IsProduction()
+        ? LogEventLevel.Error
+        : LogEventLevel.Information)
+    .CreateLogger();
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = "GitHub";
+    })
+    .AddCookie(cookieOptions => { cookieOptions.AccessDeniedPath = "/unauthorized"; })
+    .AddGitHub(authOptions =>
+    {
+        authOptions.ClientId = configuration["GithubOauth:ClientId"];
+        authOptions.ClientSecret = configuration["GITHUB_OAUTH_CLIENT_SECRET"];
+        authOptions.CallbackPath = "/callback";
+        authOptions.Scope.Add("user:email");
+    });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new()
+    {
+        Title = builder.Environment.ApplicationName,
+        Version = "v1"
+    });
+    options.AddSecurityDefinition("GithubOAuth", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Description = "Github OAuth2 with custom user whitelist",
+        In = ParameterLocation.Cookie,
+        Flows = new OpenApiOAuthFlows()
+        {
+            AuthorizationCode = new OpenApiOAuthFlow()
+            {
+                Scopes = new Dictionary<string, string>()
+                {
+                    { "user:email", "read user email from github" }
+                },
+                AuthorizationUrl = new Uri("https://github.com/login/oauth/authorize")
+            }
+        },
+    });
+    options.OperationFilter<GithubAuth.SecurityFilter>();
+});
+
+#endregion config
+
+builder.Services
+    .AddAuthorization(GithubAuth.CustomPolicy());
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{builder.Environment.ApplicationName} v1"));
+app.MapGet("/", () => "Hello World!")
+    .RequireAuthorization("ValidGithubUser");
+app.MapGet("/authorize", () => "authorized")
+    .RequireAuthorization("ValidGithubUser");
+app.MapGet("/unauthorized", Results.Unauthorized);
+
+app.Run();
