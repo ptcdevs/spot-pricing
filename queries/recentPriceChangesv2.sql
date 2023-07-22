ec8abc6e7713c99bf3ba921ed52ede9c0e39a848
David Gallmeier <dg@xounges.net>
2023-07-22 00:36:57 -0400

    TODO: make fields empty space instead of null
    rows may not joining b/c of nulls

--- queries/recentPriceChangesv2.sql
+++ queries/recentPriceChangesv2.sql
@@ -52,12 +52,13 @@ with grouped as (select ODP."Instance Type",
                             g."Operating System" = odp."Operating System" and
                             g."License Model" = odp."License Model"
                 where g."Unit" = 'Hrs'
+--                   and g."Instance Type" like 'p4%'
 --                   and g."Operating System" = 'RHEL'
 --                   and g."PurchaseOption" = 'No Upfront'
 --                   and g."Tenancy" = 'Dedicated'
 --                   and (g."LeaseContractLength" = '1yr' or g."LeaseContractLength" = '1 yr')
-                  and g.minprice <> g.maxprice),
-
+                  and g.minprice <> g.maxprice
+                ),
      recent_changes as (select "Instance Type",
                                minprice,
                                maxprice,
@@ -73,51 +74,51 @@ with grouped as (select ODP."Instance Type",
                                "License Model"
                         from report
                         where "EffectiveDate" > '2022-01-01')
-select *
-from recent_changes
--- select "Instance Type",
---        "EffectiveDate",
---        "PricePerUnit",
---        minprice,
---        maxprice,
---        setCount,
---        "Unit",
---        "TermType",
---        "LeaseContractLength",
---        "PurchaseOption",
---        "OfferingClass",
---        "Current Generation",
---        "Tenancy",
---        "Operating System",
---        "License Model"
--- from report r
--- where exists (select true
---               from recent_changes rc
---               where r."Instance Type" = rc."Instance Type"
---                 and r.minprice = rc.minprice
---                 and r.maxprice = rc.maxprice
---                 and r.setCount = rc.setCount
---                 and r."Unit" = rc."Unit"
---                 and r."TermType" = rc."TermType"
---                 and r."LeaseContractLength" = rc."LeaseContractLength"
---                 and r."PurchaseOption" = rc."PurchaseOption"
---                 and r."OfferingClass" = rc."OfferingClass"
---                 and r."Current Generation" = rc."Current Generation"
---                 and r."Tenancy" = rc."Tenancy"
---                 and r."Operating System" = rc."Operating System"
---                 and r."License Model" = rc."License Model")
--- order by "Instance Type",
---          minprice,
---          maxprice,
---          setCount,
---          "Unit",
---          "TermType",
---          "LeaseContractLength",
---          "PurchaseOption",
---          "OfferingClass",
---          "Current Generation",
---          "Tenancy",
---          "Operating System",
---          "License Model",
---          r."EffectiveDate"
---
+-- select *
+-- from recent_changes
+select "Instance Type",
+       "EffectiveDate",
+       "PricePerUnit",
+       minprice,
+       maxprice,
+       setCount,
+       "Unit",
+       "TermType",
+       "LeaseContractLength",
+       "PurchaseOption",
+       "OfferingClass",
+       "Current Generation",
+       "Tenancy",
+       "Operating System",
+       "License Model"
+from report r
+where exists (select true
+              from recent_changes rc
+              where r."Instance Type" = rc."Instance Type"
+                and r.minprice = rc.minprice
+                and r.maxprice = rc.maxprice
+                and r.setCount = rc.setCount
+                and r."Unit" = rc."Unit"
+                and r."TermType" = rc."TermType"
+                and r."LeaseContractLength" = rc."LeaseContractLength"
+                and r."PurchaseOption" = rc."PurchaseOption"
+                and r."OfferingClass" = rc."OfferingClass"
+                and r."Current Generation" = rc."Current Generation"
+                and r."Tenancy" = rc."Tenancy"
+                and r."Operating System" = rc."Operating System"
+                and r."License Model" = rc."License Model")
+order by "Instance Type",
+         minprice,
+         maxprice,
+         setCount,
+         "Unit",
+         "TermType",
+         "LeaseContractLength",
+         "PurchaseOption",
+         "OfferingClass",
+         "Current Generation",
+         "Tenancy",
+         "Operating System",
+         "License Model",
+         r."EffectiveDate"
+
